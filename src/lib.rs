2ae54f8e909fcff809b9f4b9fd73fd76d4f97f1b
zjhsd2007 <flyinksy@gmail.com>
2023-07-28 15:21:53 +0800

    chore: refine

--- src/lib.rs
+++ src/lib.rs
@@ -23,47 +23,44 @@ use anyhow::{Context, Result};
 use once_cell::sync::Lazy;
 use regex::{Captures, Regex};
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, fs, fs::File, io::BufReader, path::Path};
 use std::sync::Mutex;
+use std::{collections::HashMap, fs, fs::File, io::BufReader, path::Path};
 
-const INTER_REG: Lazy<Regex> = Lazy::new(|| Regex::new(r"%\d+").unwrap());
+static INTER_REG: Lazy<Regex> = Lazy::new(|| Regex::new(r"%\d+").unwrap());
 
-pub static I18N:Lazy<Mutex<I18n>> = Lazy::new(|| {
-    Mutex::new(I18n::new("cn"))
-});
+pub static I18N: Lazy<Mutex<I18n>> = Lazy::new(|| Mutex::new(I18n::new("cn")));
 
-pub fn set_lang(lang:&str) {
+pub fn set_lang(lang: &str) {
     let mut i18n = I18N.lock().unwrap();
     i18n.set_lang(lang);
 }
 
-pub fn set_source(path:&Path) {
+pub fn set_source(path: &Path) {
     let mut i18n = I18N.lock().unwrap();
     i18n.set_source(path);
 }
-
 
 type Namespace = String;
 
 #[derive(Debug, Clone, Default)]
 pub struct I18n {
-    pub(crate) lang:String,
+    pub(crate) lang: String,
     pub(crate) source: HashMap<String, Source>,
 }
 
 impl I18n {
-    pub fn new(lang:&str) -> I18n {
+    pub fn new(lang: &str) -> I18n {
         I18n {
-            lang:lang.to_uppercase(),
+            lang: lang.to_uppercase(),
             source: HashMap::new(),
         }
     }
 
-    pub fn set_lang(&mut self, lang:&str){
+    pub fn set_lang(&mut self, lang: &str) {
         self.lang = lang.to_uppercase();
     }
 
-    pub fn set_source(&mut self, path:&Path){
+    pub fn set_source(&mut self, path: &Path) {
         self.source = load_source(path);
     }
 
@@ -106,23 +103,20 @@ impl Source {
     }
 }
 
-
-fn load_source(path:&Path) -> HashMap<String, Source> {
+fn load_source(path: &Path) -> HashMap<String, Source> {
     let mut map = HashMap::new();
     if let Ok(dir) = fs::read_dir(path) {
-        for entry in dir {
-            if let Ok(entry) = entry {
-                let path = entry.path();
-                if !path.is_dir() {
-                    if let Some((file_name, file_type)) = path
-                        .file_name()
-                        .and_then(|f| f.to_str())
-                        .and_then(|f| f.rsplit_once('.'))
-                    {
-                        if file_type.to_lowercase() == "json".to_string() {
-                            if let Ok(source) = Source::from_path(&path) {
-                                map.insert(file_name.to_uppercase(), source);
-                            }
+        for entry in dir.flatten() {
+            let path = entry.path();
+            if !path.is_dir() {
+                if let Some((file_name, file_type)) = path
+                    .file_name()
+                    .and_then(|f| f.to_str())
+                    .and_then(|f| f.rsplit_once('.'))
+                {
+                    if file_type.to_lowercase() == *"json" {
+                        if let Ok(source) = Source::from_path(&path) {
+                            map.insert(file_name.to_uppercase(), source);
                         }
                     }
                 }
@@ -179,6 +173,6 @@ mod tests {
         set_source(Path::new("./source"));
         set_lang("en");
         dbg!(i18n!("这是一个测试"));
-        dbg!(i18n!("这是一个测试", ns="namespace1"));
+        dbg!(i18n!("这是一个测试", ns = "namespace1"));
     }
 }
